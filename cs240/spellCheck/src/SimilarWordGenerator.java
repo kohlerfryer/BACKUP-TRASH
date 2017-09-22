2b6a5e9525685476f8e6e3eb51387a51ef8deb9a
kohlerfryer <mkfryer@gmail.com>
2017-09-22 07:45:20 -0600

    modified simwordgen

--- cs240/spellCheck/src/SimilarWordGenerator.java
+++ cs240/spellCheck/src/SimilarWordGenerator.java
@@ -8,7 +8,16 @@
 public class SimilarWordGenerator {
     final Character[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 
-    private String[] generateSimilarWordsWithDeletion(String word) {
+    public static generateSimilarWords(String word){
+	String[] similarWords = new Array<string>();
+	similarWords[] = generateSimilarWordsWithDeletion(word);
+	similarWords[] = generateSimilarWordsWithTransposition(word);
+	similarWords[] = generateSimilarWordsWithAlteration(word);
+	similarWords[] = generateSimilarWordsWithInsertion(word);
+	return similarWords;
+    }
+
+    private static String[] generateSimilarWordsWithDeletion(String word) {
 	String[] deletionWords = new Array<string>();
         for(int i = 0; i < word.length(); i++){
 	    StringBuilder sb = new StringBuilder(word);
@@ -18,7 +27,7 @@ public class SimilarWordGenerator {
 	return deletionWords;
     }
 
-    private String[] generateSimilarWordsWithTransposition(String word) {
+    private static String[] generateSimilarWordsWithTransposition(String word) {
 	String[] transpositionWords = new Array<string>();
         for(int i = 0; i < word.length(); i++){
 	    StringBuilder sb = new StringBuilder(word);
@@ -31,17 +40,29 @@ public class SimilarWordGenerator {
 	return transpositionWords;
     }
 
-
-    private String[] generateSimilarWordsWithAlteration(String word) {
+    private static String[] generateSimilarWordsWithAlteration(String word) {
 	String[] alterationWords = new Array<string>();
         for(int i = 0; i < word.length(); i++){
-	    StringBuilder sb = new StringBuilder(word);
-	    char first = sb.charAt(i);
-	    char second = sb.charAt(i+1);
-	    sb.setCharAt(i,second);
-	    sb.setCharAt(i+1, first);
-	    deletionWords[] = sb.toString();
+	    for(int j = 0; j < alphabet.length; j++){
+		StringBuilder sb = new StringBuilder(word);
+		sb.setCharAt(i, alphabet[j]);
+		alterationWords[] = sb.toString();
+	    }
 	}
+
+	return alterationWords;
+    }
+
+    private static String[] generateSimilarWordsWithInsertion(String word) {
+	String[] insertionWords = new Array<string>();
+        for(int i = 0; i <= word.length(); i++){
+	    for(int j = 0; j < alphabet.length; j++){
+       		StringBuilder sb = new StringBuilder(word);
+   	        sb.insert(i, alphabet[j]);
+		insertionWords[] = sb.toString();
+	    }
+	}
+
 	return alterationWords;
     }
 
