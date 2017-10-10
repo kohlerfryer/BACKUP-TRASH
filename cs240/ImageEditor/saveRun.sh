ce5ce5c99fd11aa85f8c4a2d9336c32d9ffd2fca
kohlerfryer <mkfryer@gmail.com>
2017-10-09 21:45:52 -0600

    cleaned project

--- cs240/ImageEditor/saveRun.sh
+++
@@ -1,9 +0,0 @@
-clear
-rm src/*.class
-javac src/ImageEditor.java
-java src/ImageEditor assets/pictures/slctemple.ppm  /Users/kittykatt/Desktop/devShelf/BYU/cs240/ImageEditor/assets/picturesEdited/slcTempleGrey.ppm grayscale
-java src/ImageEditor assets/pictures/slctemple.ppm  /Users/kittykatt/Desktop/devShelf/BYU/cs240/ImageEditor/assets/picturesEdited/slcTempleInverted.ppm invert
-java src/ImageEditor assets/pictures/slctemple.ppm  /Users/kittykatt/Desktop/devShelf/BYU/cs240/ImageEditor/assets/picturesEdited/slcTempleEmbossed.ppm emboss
-
-
-open /Users/kittykatt/Desktop/devShelf/BYU/cs240/ImageEditor/assets/picturesEdited/slcTempleEmbossed.ppm
--- cs240/spellCheck/run.sh
+++
@@ -1,4 +0,0 @@
-rm src/*.class
-clear
-javac src/Main.java
-java src/Main assets/words.txt ohuset
